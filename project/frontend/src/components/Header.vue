3db2dd5eb8ca40f60f857809cc760f142d7ae3bf
SamWatts5 <2508240@dundee.ac.uk>
2025-03-06 15:23:27 +0000

    logo

---
+++ project/frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <img src="">
+    <img src="">
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
---
+++ project/frontend/src/components/SidePanel.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>hi!</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
