b694256c9fd2a74830ca070f3233821d6cf5b2af
jharrisonreed <2571106@dundee.ac.uk>
2025-03-06 16:09:17 +0000

    Changed trackmate image and testing left music bar

---
+++ project/frontend/src/components/yourMusicBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+     <p>Test</p>   
+    </div>
+</template>
+
+<style scoped>
+div{
+    width:35%;
+    margin-left:20px;
+    margin-top:20px;
+    background-color: antiquewhite;
+}
+
+p{
+    height:200px;
+}
+</style>
--- project/frontend/src/views/HomeView.vue
+++ project/frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import LogoBar from "../components/logoBar.vue"
+import YourMusicBar from "../components/yourMusicBar.vue"
 </script>
 
 <template>
   <LogoBar />
+
+  <YourMusicBar />
+
   <div>
     <h1 class="text-5xl">Testing</h1>
     
