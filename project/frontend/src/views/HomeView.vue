1c3909522fe5b6371f22609955d8c7804bb44313
SamWatts5 <2508240@dundee.ac.uk>
2025-03-06 15:11:28 +0000

    feat: add new component and integrate into HomeView; remove unused icons and components

--- project/frontend/src/views/HomeView.vue
+++ project/frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import Component from "../components/component.vue"
 </script>
 
 <template>
   <div>
     <h1 class="text-5xl">Testing</h1>
+    <Component />
   </div>
 </template>
