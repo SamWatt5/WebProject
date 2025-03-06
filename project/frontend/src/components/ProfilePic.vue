424beb04dee20c93408e5b04f56d38c5f382c9aa
SamWatts5 <2508240@dundee.ac.uk>
2025-03-06 15:46:14 +0000

    feat: restructure components by removing Header.vue, adding ProfilePic.vue, and updating logoBar.vue; implement new router structure

---
+++ project/frontend/src/components/ProfilePic.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <img src="favicon.ico">
+</template>
+
+<style scoped></style>
--- project/frontend/src/components/logoBar.vue
+++ project/frontend/src/components/logoBar.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
+import ProfilePic from './ProfilePic.vue';
 </script>
 
 <template>
-    <div>
-        
-    </div>>
+    <div class="bg-black h-24 flex items-center">
+        <img src="/TrackMates.png" alt="logo" class="h-full object-cover ml-4" />
+        <div class="flex-grow"></div>
+        <div class="mr-4">
+            <ProfilePic />
+        </div>
+    </div>
 </template>
 
-<style scoped>
-    div {
-        background-color: black;
-        height:100px;
-    }
-</style>
+<style scoped></style>
