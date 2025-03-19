04f69c10973cbcf552be46353819f4ddffbe4a2a
CallumLaidlaw <2569663@dundee.ac.uk>
2025-03-19 10:55:16 +0000

    blah blah blah

---
+++ project/frontend/src/components/PlaylistCard.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import ListeningToCard from './ListeningToCard.vue';
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from './ui/dialog';
+
+
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger asChild>
+            <ListeningToCard />
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>TEST</DialogHeader>
+        </DialogContent>
+    </Dialog>
+</template>
