405379b80c8b22e8c7e97be5a0cec1ea21bb6de8
CallumLaidlaw <2569663@dundee.ac.uk>
2025-03-26 13:51:11 +0000

    frontend looking sazzy now

--- project/frontend/src/components/FriendManage.vue
+++ project/frontend/src/components/FriendManage.vue
@@ -5,11 +5,17 @@ import { Button } from "./ui/button";
 import { toast } from "vue-sonner";
 import { useFriends } from "@/stores/friends";
 import { storeToRefs } from "pinia";
+import { Card, CardHeader, CardTitle, CardDescription } from "./ui/card";
+import { CalendarDays } from "lucide-vue-next";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import Spotify from "./icons/Spotify.vue";
+import { defineProps } from "vue";
 
 defineProps({
     userName: String,
     userAvatar: String,
-    userJoined: String
+    userJoined: String,
+    spotify_id: String
 })
 
 let store = useFriends();
@@ -61,15 +67,40 @@ const unfriend = async (username) => {
 
 <template>
     <Dialog>
-        <DialogTrigger as-child>
-            <UserCard :name="userName" :avatar="userAvatar" :joined="userJoined" />
-        </DialogTrigger>
+        <!-- <DialogTrigger as-child> -->
+            <!-- <UserCard :spotify_id="spotify_id" :name="userName" :avatar="userAvatar" :joined="userJoined" /> -->
+            <Card class="cursor-pointer">
+                <CardHeader class="flex flex-row">
+                    <Avatar class="w-20 h-20 mt-2">
+                        <AvatarFallback class="text-3xl">{{ userAvatar }}</AvatarFallback>
+                    </Avatar>
+                    <div class="flex flex-col pl-3">
+                        <CardTitle class="text-4xl">@{{ userName }}</CardTitle>
+                        <CardDescription class="flex flex-row">
+                            <CalendarDays class="h-8 w-8 mt-1" /><span class="mt-[3px] text-lg ml-1"> Joined {{ userJoined
+                                }}</span>
+                        </CardDescription>
+                        <!-- <<RouterLink as-child :href="`https://open.spotify.com/user/${name}`">
+                <Spotify class="mt-2 cursor-pointer" />
+            </RouterLink>> -->
+                       <div class="flex flex-row justify-between mt-2">
+                        <a :href="`https://open.spotify.com/user/${spotify_id}`" target="_blank">
+                            <Spotify class="cursor-pointer mt-2" />
+                        </a>
+                        <DialogTrigger as-child>
+                            <Button class="mt-2">Manage</Button>
+                        </DialogTrigger>
+                       </div>
+                    </div>
+                </CardHeader>
+            </Card>
+        <!-- </DialogTrigger> -->
         <DialogContent>
             <DialogHeader>
                 <DialogTitle>Manage <span class="text-cyan-500 font-semibold">{{ userName }}</span></DialogTitle>
                 <DialogDescription>All buttons pressed on this menu are final and can't be reversed</DialogDescription>
             </DialogHeader>
-            
+
             <DialogFooter>
                 <DialogClose asChild>
                     <Button variant="destructive" @click="unfriend(userName)">Unfriend</Button>
--- project/frontend/src/components/UserCard.vue
+++ project/frontend/src/components/UserCard.vue
@@ -5,14 +5,11 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Spotify from "@/components/icons/Spotify.vue";
 import { string } from "zod";
 
-const spotifyClick = () => {
-    console.log("Spotify clicked");
-}
-
 defineProps({
     name: String,
     avatar: String,
-    joined: String
+    joined: String,
+    spotify_id: String
 })
 </script>
 
@@ -25,7 +22,12 @@ defineProps({
         <div class="flex flex-col pl-3">
             <CardTitle class="text-4xl">@{{ name }}</CardTitle>
             <CardDescription class="flex flex-row"><CalendarDays class="h-8 w-8 mt-1" /><span class="mt-[3px] text-lg ml-1"> Joined {{ joined }}</span></CardDescription>
-            <Spotify class="mt-2 cursor-pointer" @click="spotifyClick" />
+            <!-- <<RouterLink as-child :href="`https://open.spotify.com/user/${name}`">
+                <Spotify class="mt-2 cursor-pointer" />
+            </RouterLink>> -->
+            <a :href="`https://open.spotify.com/user/${spotify_id}`" target="_blank" class="mt-2">
+                <Spotify class="cursor-pointer mt-2" />
+            </a>
         </div>
         </CardHeader>
     </Card>
