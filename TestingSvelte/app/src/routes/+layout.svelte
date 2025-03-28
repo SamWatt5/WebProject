6cfb7421cc90690d7edf1b53028f8709a8630f34
Callum Laidlaw <149398753+CallumLaidlaw@users.noreply.github.com>
2025-03-28 00:35:37 +0000

    Delete TestingSvelte/app directory

--- TestingSvelte/app/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import "../app.css";
-    import NavBar from "../components/NavBar.svelte";
-    let { children } = $props();
-</script>
-
-<NavBar />
-{@render children()}
--- TestingSvelte/app/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-    <a href="/about">About</a>
-</div>
